81be1f77de80cdde9e75862e6b938911e9c9662c
Florian Arndt <post@florianarndt.com>
2021-04-18 15:23:14 +0200

    - better test for AttachmentEventsReader

--- tests/lib/attachment_events_reader/attachment_events_reader.py
+++ tests/lib/attachment_events_reader/attachment_events_reader.py
@@ -3,19 +3,21 @@ from lib.attachment_events_reader import AttachmentEventsReader
 
 
 class AttachmentEventsReaderTest(unittest.TestCase):
-    # TODO: write this test
     def test_reader(self):
         reader = AttachmentEventsReader()
+        # set user
         message = {
             'from': 'user1@example.com'
         }
+        # add three events
         attachment_payload_lines = [
             'Timeline,Start,Stop,Title,Note',
-            '"Project A","2021-04-17 13:50:00","","2021-04-17 16:30","Task 57","Done with bells and whistles."',
-            '"Project B","2021-04-16 06:00:00","","2021-04-16 08:00","Task 13","Almost done, but still WiP."',
+            '"Project A (#1)","2021-04-17 13:50:00","","2021-04-17 16:30","Task 57","Done with bells and whistles."',
+            '"Project B (#2)","2021-04-16 06:00:00","","2021-04-16 08:00","Task 13","Almost done, but still WiP."',
+            '"Project C","2021-04-16 06:00:00","","2021-04-16 08:00","Task 13","Almost done, but still WiP."',
         ]
         attachment_payload = attachment_payload_lines
         reader.add(message, bytes("\n".join(attachment_payload), 'utf-8'))
 
-        # expect two timespans
-        self.assertEqual(2, len(reader.events))
+        # expect the events read from the CSV above
+        self.assertEqual(3, len(reader.events))
